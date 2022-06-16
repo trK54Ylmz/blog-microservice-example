c89373a47a4bcf66c3b1c5184ab036c54babb137
Tarık Yılmaz <tarikyilmaz@yandex.com.tr>
2022-06-16 12:49:31 +0300

    Update bash command

--- run.sh
+++ run.sh
@@ -11,3 +11,12 @@ protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. \
 python3 -m grpc_tools.protoc --proto_path=./proto --python_out=./front/proto --grpc_python_out=./front/proto proto/article/article.proto
 
 python3 -m grpc_tools.protoc --proto_path=./proto --python_out=./front/proto --grpc_python_out=./front/proto proto/user/user.proto
+
+if [[ $(uname -s) == Darwin* ]];
+then
+    sed -i '' 's/from article import/from . import/1' front/proto/article/article_pb2_grpc.py
+    sed -i '' 's/from user import/from . import/1' front/proto/user/user_pb2_grpc.py
+else
+    sed -i 's/from article import/from . import/1' front/proto/article/article_pb2_grpc.py
+    sed -i 's/from user import/from . import/1' front/proto/user/user_pb2_grpc.py
+fi
